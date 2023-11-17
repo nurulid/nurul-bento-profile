c35a9c096be7f6a5a41d71447f790443e8efb5c7
Nurul Insani Dewi <nurulinsanid@gmail.com>
2023-11-18 02:12:39 +0700

    add content and style improvement

---
+++ README.md
@@ -0,0 +1,11 @@
+# Nurul Personal Website
+
+## Design sources reference:
+- https://dribbble.com/shots/22902217-Bento-CV-Framer-Template
+- https://dribbble.com/shots/22958610-Bento-Grid-Illustrations
+- https://dribbble.com/shots/20869685-Copilot-bento-box
+- https://x.com/60fpsdesign/status/1724127814415454651?s=20
+
+Icons from https://fontawesome.com/
+
+Made with Tailwind CSS.
--- main.js
+++ main.js
@@ -1,7 +1,64 @@
 import Typed from "typed.js";
+import confetti from "canvas-confetti";
 
 const typed = new Typed("#intro", {
-  strings: ["Welcome", "Thank you for visiting."],
+  strings: ["Welcome.", "Thank you for visiting.", "Have a wonderful day."],
   typeSpeed: 50,
   loop: true,
 });
+
+const appreciateBtn = document.getElementById("appreciate");
+appreciateBtn.addEventListener("click", () => {
+  const myCanvas = document.createElement("canvas");
+  myCanvas.style.position = "fixed";
+  myCanvas.style.inset = 0;
+  myCanvas.style.width = "100vw";
+  myCanvas.style.height = "100vh";
+  myCanvas.style.zIndex = 1;
+  document.body.appendChild(myCanvas);
+
+  const myConfetti = confetti.create(myCanvas, {
+    resize: true,
+    useWorker: true,
+  });
+  myConfetti({
+    particleCount: 100,
+    startVelocity: 30,
+    spread: 360,
+    origin: {
+      x: Math.random(),
+      // since they fall down, start a bit higher than random
+      y: Math.random() - 0.2
+    }
+  });
+});
+
+// do this for 1 seconds
+var duration = 1 * 1000;
+var end = Date.now() + duration;
+
+// setTimeout(function() {
+  (function frame() {
+    // launch a few confetti from the left edge
+    confetti({
+      particleCount: 7,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 }
+    });
+    // and launch a few from the right edge
+    confetti({
+      particleCount: 7,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 }
+    });
+
+    // keep going until we are out of time
+    if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+  }
+  ());
+// }, duration);
+
