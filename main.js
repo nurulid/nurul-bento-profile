16d0964631226413bcb64f0a6a649ba81a634477
Nurul Insani Dewi <nurulinsanid@gmail.com>
2023-11-17 12:25:10 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#intro", {
+  strings: ["Welcome", "Thank you for visiting."],
+  typeSpeed: 50,
+  loop: true,
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      padding: {
+        '0.5': '1px',
+      }
+    },
+
+  },
+  plugins: [],
+}
+
